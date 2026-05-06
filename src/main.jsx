1f4289d4809cdccd0f4df0aab36dba8798d25981
DavinderSmagh <davindersinghsmagh3630@gmail.com>
2026-05-06 15:03:56 +0530

    Fix mobile layout and main hook usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,35 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Lenis from 'lenis'
 
+function LenisProvider({ children }) {
+  React.useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => lenis.destroy()
+  }, [])
+
+  return children
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LenisProvider>
+      <App />
+    </LenisProvider>
   </React.StrictMode>,
 )
-
-// Smooth scrolling (Lenis)
-useEffect(() => {
-  const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    smoothWheel: true,
-    smoothTouch: false,
-  })
-
-  function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-  }
-
-  requestAnimationFrame(raf)
-
-  return () => lenis.destroy()
-}, [])
